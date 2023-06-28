b046d1ad1a0f4806c9d75a566670d34070c1cc46
O2theC <93496386+O2theC@users.noreply.github.com>
2023-06-27 23:02:40 -0400

    Update CrabChampionSaveManager.py
    updated comments and info section
    you no longer need to put script in certain folder unless it is in a custom install location

--- CrabChampionSaveManager.py
+++ CrabChampionSaveManager.py
@@ -4,49 +4,34 @@ import shutil
 import time
 
 def extract_numbers(input_string):
+    """Converts the input string to an integer if possible, otherwise returns -1."""
     try:
         return int(input_string)
     except:
         return -1
     
 def is_valid_folder_name(folder_name):
+    """Checks if the folder name is valid based on certain criteria.
+    
+    The folder name should not contain any of the characters \\/:*?\"<>|,
+    should not end in a period or consist of only spaces and periods,
+    and should not be any of the reserved folder names (SaveGames, Logs, Config).
+    
+    Returns True if the folder name is valid, False otherwise.
     """
-    Checks if a folder name is valid.
-
-    A valid folder name must satisfy the following conditions:
-    1. Does not contain any of the characters \\/:*?\"<>|
-    2. Does not end in a period or consist of only spaces and periods
-    3. Is not one of the reserved folder names (SaveGames, Logs, Config)
-
-    Args:
-    - folder_name (str): The name of the folder
-
-    Returns:
-    - bool: True if the folder name is valid, False otherwise
-    """
-    # Define the regular expression pattern for a valid folder name
     pattern = r'^[^\\/:*?"<>|]+$'
-
-    # Check if the folder name matches the pattern and is not empty or made of only spaces/periods
     return bool(re.match(pattern, folder_name)) and not folder_name.strip(" .") == "" and not folder_name == "SaveGames" and not folder_name == "Logs" and not folder_name == "Config"
     
 def backupSave():
+    """Performs the backup of the save game.
+    
+    Asks the user for the backup name, validates it, and creates a backup
+    by copying the SaveGames folder to the specified backup location.
+    If a backup with the same name already exists, prompts for overwrite confirmation.
     """
-    Creates a backup of the SaveGames folder.
 
-    The function prompts the user to enter a name for the backup.
-    The backup name must be a valid folder name and should not already exist.
-    If a valid backup name is provided, the SaveGames folder is copied to the backup location.
-
-    Returns:
-    - None
-    """
     current_directory = os.getcwd()
-    # List all the items in the current directory
-    items = os.listdir(current_directory)
-
-    # Filter out only the folders 
-    folders = [item for item in items if os.path.isdir(os.path.join(current_directory, item))]
+    folders = getBackups()
     confirm = False
     while(not confirm):
         saveName = input("What should the backup be named\nName can not contain any of these characters \\/:*?\"<>|\ncan not end in a period or consist of only spaces and periods\ncan not be any of the games folder (SaveGames , Logs , Config)\nEnter None to not make a backup\n")
@@ -82,25 +67,15 @@ def backupSave():
     
 
 def restoreBackup():
+    """Restores a backup of the save game.
+    
+    Displays the available backups and prompts the user to choose one.
+    If a backup is selected, it replaces the current SaveGames folder
+    with the contents of the chosen backup.
     """
-    Restores a backup of the SaveGames folder.
 
-    The function prompts the user to choose a backup to restore.
-    If a valid backup is selected, the current SaveGames folder is deleted
-    and the selected backup is copied to the SaveGames location.
-
-    Returns:
-    - None
-    """
     current_directory = os.getcwd()
-    # List all the items in the current directory
-    items = os.listdir(current_directory)
-
-    # Filter out only the folders
-    folders = [item for item in items if os.path.isdir(os.path.join(current_directory, item))]
-    folders.remove("SaveGames")
-    folders.remove("Config")
-    folders.remove("Logs")
+    folders = getBackups()
     print("Choose Backup to restore")
     print("0 - don't restore any backup")
     for i in range(len(folders)):
@@ -130,42 +105,39 @@ def restoreBackup():
         input("Press Enter to continue . . .")
 
 def editBackup():
+    """Edits a backup of the save game.
+    
+    Displays the available backups and prompts the user to choose one.
+    If a backup is selected, it opens the SaveSlot.sav file for editing
+    using the uesave tool. Two backup copies (SaveSlotBackupA.sav and SaveSlotBackupB.sav)
+    are created before editing.
     """
-    Edits a backup of the SaveGames folder.
 
-    The function prompts the user to choose a backup to edit.
-    If a valid backup is selected, the SaveSlot.sav file within the backup is edited using a third-party tool.
-    Two backup copies (SaveSlotBackupA.sav and SaveSlotBackupB.sav) are created before editing.
-
-    Returns:
-    - None
-    """
     current_directory = os.getcwd()
-    # List all the items in the current directory
-    items = os.listdir(current_directory)
-
-    # Filter out only the folders
-    folders = [item for item in items if os.path.isdir(os.path.join(current_directory, item))]
-    folders.remove("SaveGames")
-    folders.remove("Config")
-    folders.remove("Logs")
+    folders = getBackups()
     print("Choose Backup to edit")
     print("0 - don't edit any backup")
+    print("1 - edit current save")
     for i in range(len(folders)):
-        print((i+1),"-",folders[i])
+        print((i+2),"-",folders[i])
     choice = input("")
     choice = extract_numbers(choice)
-    while(int(choice)<0 or int(choice)>len(folders)):
+    while(int(choice)<0 or int(choice)>(len(folders)+1)):
         print("\nInvalid choice. Valid choices are")
         print("0 - don't edit any backup")
+        print("1 - edit current save")
         for i in range(len(folders)):
-            print((i+1),"-",folders[i])
+            print((i+2),"-",folders[i])
         choice = input("")
     if(extract_numbers(choice) == 0):
         print("Not editing any backup")
         time.sleep(1)
         return
-    saveFile = os.path.join(current_directory,str(folders[extract_numbers(choice)-1]))
+    saveFile = ""
+    if(extract_numbers(choice) == 1):
+        saveFile = os.path.join(current_directory,"SaveGames")
+    else:
+        saveFile = os.path.join(current_directory,str(folders[extract_numbers(choice)-2]))
     saveFile = os.path.join(saveFile,"SaveSlot.sav")
     saveBackA = saveFile.replace("SaveSlot.sav","SaveSlotBackupA.sav")
     saveBackB = saveFile.replace("SaveSlot.sav","SaveSlotBackupB.sav")
@@ -181,24 +153,14 @@ def editBackup():
     shutil.copy(sf, saveBackB)
     
 def deleteBackup():
+    """Deletes a backup of the save game.
+    
+    Displays the available backups and prompts the user to choose one.
+    If a backup is selected, it permanently deletes the corresponding backup folder.
     """
-    Deletes a backup of the SaveGames folder.
-
-    The function prompts the user to choose a backup to delete.
-    If a valid backup is selected, the backup folder is deleted.
-
-    Returns:
-    - None
-    """
+    
     current_directory = os.getcwd()
-    # List all the items in the current directory
-    items = os.listdir(current_directory)
-
-    # Filter out only the folders
-    folders = [item for item in items if os.path.isdir(os.path.join(current_directory, item))]
-    folders.remove("SaveGames")
-    folders.remove("Config")
-    folders.remove("Logs")
+    folders = getBackups()
     print("Choose Backup to delete")
     print("0 - don't delete any backup")
     for i in range(len(folders)):
@@ -225,13 +187,70 @@ def deleteBackup():
         print(error)
         input("Press Enter to continue . . .")
 
+def listBackups():
+    """Lists all the available backups of the save game.
+    
+    Retrieves the list of backup folders and displays them to the user.
+    """
+    
+    current_directory = os.getcwd()
+    items = os.listdir(current_directory)
+    folders = getBackups()
+    print("Current Backups:")
+    for i in folders:
+        print(i)
+    print(len(folders),"Backups stored")
+    input("Press Enter to continue . . .")
+
+
+def getBackups():
+    """Retrieves the list of backup folders.
+    
+    Searches the current directory for backup folders and returns a list of their names.
+    """
+    
+    current_directory = os.getcwd()
+    items = os.listdir(current_directory)
+    try:
+        items.remove("SaveGames")
+        items.remove("Config")
+        items.remove("Logs")
+    except:
+        None
+    folders = [item for item in items if os.path.isdir(os.path.join(current_directory, item))]
+    folders = [item for item in items if os.path.isfile(os.path.join(os.path.join(current_directory, item),"SaveSlot.sav"))]
+    return folders
+
+def currentDirCheck():
+    """Checks if the required folders are present in the current directory.
+    
+    Checks if the folders SaveGames, Logs, and Config exist in the current directory.
+    Returns True if any of the folders is missing, indicating a directory check failure.
+    Returns False if all the required folders are present.
+    """
+    
+    folder_names = ["SaveGames", "Logs", "Config"]
+    for folder_name in folder_names:
+        folder_path = os.path.join(os.getcwd(), folder_name)
+        if os.path.exists(folder_path) and os.path.isdir(folder_path):
+            None
+        else:
+            return True
+    return False      
+            
+            
+if(currentDirCheck()):
+    
+    new_dir = os.path.expandvars("%APPDATA%\\..\\Local\\CrabChampions\\Saved")
+    os.chdir(new_dir)
+
 print("Welcome to Crab Champion Save Manager")
 print("Made By O2C, GitHub repo at https://github.com/O2theC/CrabChampionSaveManager")
 while(True):
-    choice = input("What do you want to do\n1 - Edit save game (requires https://github.com/trumank/uesave-rs with Rust installed)\n2 - Backup save\n3 - Restore save from backup (Warning : Deletes current save)\n4 - Delete backup\n5 - Info/How to use\n6 - Exit\n")
+    choice = input("What do you want to do\n1 - Edit save game (requires https://github.com/trumank/uesave-rs with Rust installed)\n2 - Backup save\n3 - Restore save from backup (Warning : Deletes current save)\n4 - Delete backup\n5 - List Backups\n6 - Info/How to use\n7 - Exit\n")
     choice = extract_numbers(choice)
-    while(extract_numbers(choice)<1 or extract_numbers(choice)>6):
-        choice = input("Invalid choice. Valid choices are\n1 - Edit save game (requires https://github.com/trumank/uesave-rs with Rust installed\n2 - Backup Save\n3 - Restore Save from backup (Warning : Deletes current save)\n4 - Delete backup\n5 - Info/How to use\n6 - Exit\n")
+    while(extract_numbers(choice)<1 or extract_numbers(choice)>7):
+        choice = input("Invalid choice. Valid choices are\n1 - Edit save game (requires https://github.com/trumank/uesave-rs with Rust installed\n2 - Backup Save\n3 - Restore Save from backup (Warning : Deletes current save)\n4 - Delete backup\n5 - List Backups\n6 - Info/How to use\n7 - Exit\n")
     choice = extract_numbers(choice)
     if(choice == 1):
         editBackup()
@@ -246,36 +265,54 @@ while(True):
         deleteBackup()
         print("\n")
     elif(choice == 5):
+        listBackups()
+        print("\n")
+    elif(choice == 6):
         print("Crab Champion Save Manager\n")
+        print("Welcome to Crab Champion Save Manager, a script designed to help you manage your save files for the game Crab Champion.")
         print("Made By O2C, GitHub repo at https://github.com/O2theC/CrabChampionSaveManager")
-        print("This program helps you manage your save files for the game Crab Champion.\n")
+        print("This program provides the following options:\n")
+        
         print("1. Edit Save Game:")
         print("   - This option allows you to edit your save game using the uesave tool.")
-        print("   - You will need to have the uesave tool (https://github.com/trumank/uesave-rs) and Rust installed.")
+        print("   - Before choosing this option, make sure you have the uesave tool (https://github.com/trumank/uesave-rs) and Rust installed.")
         print("   - Select a backup to edit, and the tool will open the SaveSlot.sav file for editing.")
         print("   - Two backup copies (SaveSlotBackupA.sav and SaveSlotBackupB.sav) will be created before editing.")
-        print("   - Make sure to follow the uesave documentation for editing the save file.")
-        print("   - Please note that editing the save game can lead to unexpected behavior or corrupt saves, so proceed with caution.")
+        print("   - Note: Editing the save game can lead to unexpected behavior or corrupt saves, so proceed with caution.")
+
         print("\n2. Backup Save:")
         print("   - This option allows you to create a backup of your current save game.")
-        print("   - Enter a name for the backup folder when prompted.")
+        print("   - When prompted, enter a name for the backup folder.")
         print("   - The backup name must be a valid folder name and should not already exist.")
         print("   - If a valid backup name is provided, the SaveGames folder will be copied to the backup location.")
+
         print("\n3. Restore Save from Backup:")
         print("   - This option allows you to restore a backup of your save game.")
         print("   - Select a backup from the available options.")
         print("   - The current SaveGames folder will be deleted, and the selected backup will be copied to the SaveGames location.")
-        print("   - Please note that this action will overwrite your current save game, so make sure to choose the correct backup.")
+        print("   - Caution: Restoring a backup will overwrite your current save game, so choose the backup carefully.")
+
         print("\n4. Delete Backup:")
         print("   - This option allows you to delete a backup of your save game.")
         print("   - Select a backup from the available options.")
         print("   - The selected backup folder will be permanently deleted.")
-        print("   - Please note that this action cannot be undone, so be careful when deleting backups.")
-        print("\n5. Info/How to use:")
+        print("   - Note: Deleting a backup cannot be undone, so be careful when removing backups.")
+
+        print("\n5. List Backups:")
+        print("   - This option displays the available backups of your save game.")
+        print("   - It shows the names of the backup folders and the total count of backups.")
+
+        print("\n6. Info/How to use:")
         print("   - This option provides a brief description of each functionality and how to use them.")
-        print("\n6. Exit:")
+        print("   - It gives an overview of the script's purpose and directs you to the GitHub repository for reporting issues and suggestions.")
+
+        print("\n7. Exit:")
         print("   - This option allows you to exit the program.")
-        print("\n")
-    elif(choice == 6):
+        
+        print("Note that for this script to edit save files it need uesave from https://github.com/trumank/uesave-rs\n it is written in rust so you will need to get that")
+        print("Report issues and suggestions to https://github.com/O2theC/CrabChampionSaveManager")
+        print("Note that this info section and most of the code comments were made by chatgpt , if it did something wrong, make an issue at https://github.com/O2theC/CrabChampionSaveManager")
+        print("\nPress Enter to continue . . .")
+    elif(choice == 7):
         print("Exiting...")
         break
