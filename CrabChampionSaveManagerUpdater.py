29bf617980fcafdf22327a14c9b1ae70a7826227
O2theC <93496386+O2theC@users.noreply.github.com>
2023-07-29 18:17:35 -0400

    Update CrabChampionSaveManagerUpdater.py

--- CrabChampionSaveManagerUpdater.py
+++ CrabChampionSaveManagerUpdater.py
@@ -2,18 +2,26 @@ import os
 import subprocess
 import time
 
-if(not os.path.exists("CrabChampionSaveManagerUpdated.exe")):
+if not os.path.exists("CrabChampionSaveManagerUpdated.exe"):
     print("Update Error: Invalid execution of updater.exe.")
-    print("Please do not run updater.exe manually. It is automatically executed by the main program.")
+    print(
+        "Please do not run updater.exe manually. It is automatically executed by the main program."
+    )
     exit(2)
-
-time.sleep(2)
+print("\nUpdating CCSM\nThis may take a few minutes\n3/4")
 try:
     os.remove("CrabChampionSaveManager.exe")
 except:
+    # f = open("no .exe", "w")
+    # f.write("s")
+    # f.close()
     None
 try:
-    os.rename("CrabChampionSaveManagerUpdated.exe","CrabChampionSaveManager.exe")
+    os.rename("CrabChampionSaveManagerUpdated.exe", "CrabChampionSaveManager.exe")
 except:
+    # f = open("no update.exe", "w")
+    # f.write("s")
+    # f.close()
     None
-subprocess.Popen(["CrabChampionSaveManager.exe"], shell=True)
+
+os.system("start CrabChampionSaveManager.exe")
